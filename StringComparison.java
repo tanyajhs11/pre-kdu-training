537c864e7062780b1b32112456ada00ffcac5237
Tanya Agarwal <tanyajhs11@gmail.com>
2023-11-29 16:56:04 +0530

    java code

--- StringComparison.java
+++ StringComparison.java
@@ -2,35 +2,35 @@ import java.util.Scanner;
 
 public class StringComparison {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Input two strings
         System.out.print("Enter the first string: ");
-        String firstString = scanner.nextLine();
+        String string1 = sc.nextLine();
 
         System.out.print("Enter the second string: ");
-        String secondString = scanner.nextLine();
+        String string2 = sc.nextLine();
 
         // Print length of the first string
-        System.out.println("The length of the first string is " + firstString.length());
+        System.out.println("The Length of the first string is " + string1.length());
 
         // Print length of the second string
-        System.out.println("The length of the second string is " + secondString.length());
+        System.out.println("The Length of the second string is " + string2.length());
 
         // Check if the lengths are the same
-        if (firstString.length() == secondString.length()) {
-            System.out.println("The length of the two strings is the same");
+        if (string1.length() == string2.length()) {
+            System.out.println("The length of the two strings is same");
         } else {
-            System.out.println("The length of the two strings is not the same");
+            System.out.println("The length of the two strings is not  same");
         }
 
         // Check if the strings are equal
-        if (firstString.equals(secondString)) {
+        if (string1.equals(string2)) {
             System.out.println("Two strings are equal");
         } else {
             System.out.println("Two strings are not equal");
         }
 
-        scanner.close();
+        sc.close();
     }
 }
